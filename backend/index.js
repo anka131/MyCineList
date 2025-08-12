8bd8c5cee27bf9d33b5fcbbbbfa8e3ae011512ed
anka131 <ankaknezevic99@gmail.com>
2025-08-12 12:35:53 +0200

    change

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'https://cine-list-phi.vercel.app/',
+  origin: 'http://localhost:3000',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   exposedHeaders: ['Authorization'],
